c1f2ae2cfc29bd3dd0aa101a0fc42fe3771a7a52
zhouheling <zhouhl@jjmmw.com>
2015-11-25 10:09:14 +0800

    按照6.3章节修改了功能测试

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(LiveServerTestCase):
-    
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -19,43 +19,72 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # 用户查看应用首页
+        # 伊迪丝听说有一个很酷的在线待办事项应用
+        # 她去看了这个应用的首页
         self.browser.get(self.live_server_url)
 
-        # 用户注意到网页标题和头部都包含了"To-Do"这个词
+        # 她注意到网页的标题和头部都包含"To-Do"这个词
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # 应用邀请用户输入一个代办事项
+        # 应用邀请她输入一个代办事项
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        # 用户在文本框输入了'Buy peacock feathers'
+        # 她在一个文本框中输入了'Buy peacock feathers'（购买孔雀羽毛）
+        # 伊迪丝的爱好是使用假蝇做鱼饵钓鱼
         inputbox.send_keys('Buy peacock feathers')
 
-        # 按回车键后，页面更新了
-        # 代办事项表格中显示了'1: Buy peacock feathers'
+        # 她按回车后，被待到一个新的URL
+        # 这个页面的待办事项清单中显示了'1: Buy peacock feathers'
         inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegexpMatches(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 页面又显示了一个文本框，可以输入其他的代办事项
-        # 用户输入了"Use peacock feathers to make a fly"
+        # 她输入了"Use peacock feathers to make a fly"（使用孔雀羽毛做假蝇）
+        # 伊迪丝做事很有条理
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
-        # 页面再次更新，清单中显示了这两个代码事项
+        # 页面再次更新，她的清单中显示了这两个代码事项
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # 用户想知道网站是否记住他的清单
-        # 用户看到网站为他生成了唯一的URL
-        # 页面中有一些文字解说这个功能
-        self.fail('Finish the test!')
+        # 现在一个叫做弗朗西斯的新用户访问了网站
 
-        # 用户访问了那个URL，发现办事项清单还在
+        ## 我们使用了一个新浏览器会话
+        ## 确保伊迪丝的信息不会从cookie中泄露出来
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
 
+        # 弗朗西斯访问首页
+        # 页面中看不到伊迪丝的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # 弗朗西斯输入一个新待办事项，新建一个清单
+        # 他不像伊迪丝那样兴趣盎然
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # 弗朗西斯获得了他的唯一URL
+        francis_list_url = self.browser.current_url
+        self.assertRegexpMatches(francis_list_url, '/list/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 这个页面还是没有伊迪丝的清单
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # 两人都很满意，去睡觉了
